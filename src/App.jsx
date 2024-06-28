4432ddfd2114aa230cfc8bbd9620661e9a2e91cd
Priyanshu Maithani <priyanshumaithani02@gmail.com>
2024-06-28 09:17:24 +0530

    added ussestate

--- src/App.jsx
+++ src/App.jsx
@@ -6,35 +6,30 @@ import "./App.css";
 import FoodInput from "./components/FoodInput";
 import { useState } from "react";
 
-
 function App() {
   const [foodItems, setFoodItems] = useState([]);
 
-  const onKeyDown = (event) =>{
-    if(event.key === "Enter"){
+  const onKeyDown = (event) => {
+    if (event.key === "Enter") {
       let newFoodItem = event.target.value;
       event.target.value = "";
       let newItems = [...foodItems, newFoodItem];
       setFoodItems(newItems);
     }
-  }
+  };
 
-  let [textToShow, setTextState]  = useState();
+  let [textToShow, setTextState] = useState();
 
   //console.log(`Current value of textState: ${textToShow}`);
 
   return (
     <>
-    <Container>
-      <h1 className="food-heading">Healthy Food</h1>
-      <FoodInput handleKeyDown = {onKeyDown}></FoodInput>
-      <ErrorMsg itmes={foodItems}> </ErrorMsg>
-      <FoodItems itmes={foodItems}/> 
-    </Container>
-
-    {/* <Container>
-      <p>Above is the list of food items that is healthy for well living.</p>
-    </Container> */}
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
+        <FoodInput handleKeyDown={onKeyDown}></FoodInput>
+        <ErrorMsg itmes={foodItems}> </ErrorMsg>
+        <FoodItems itmes={foodItems} />
+      </Container>
     </>
   );
 }
